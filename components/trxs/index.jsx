3392b033a0b1ac8ef4be2a96b14f0dfd0d3bcf9d
tmdme <han@outblock.io>
2023-03-01 15:19:51 +1100

    update file

--- components/trxs/index.jsx
+++ components/trxs/index.jsx
@@ -17,7 +17,7 @@ import { useTranslation } from 'next-i18next'
 import { useTrxs } from 'api/query'
 import { timeformater, ellipseStr } from 'utils'
 import moment from 'moment'
-import { t } from '@onflow/fcl'
+import LoadingPanel from '../loadingPanel'
 
 export default function Comp(props) {
   const { t } = useTranslation()
@@ -40,50 +40,57 @@ export default function Comp(props) {
     pages.length > 0 ? pages[pages.length - 1].totalPages : false
 
   return (
-    <TableContainer>
-      <Table variant="striped" colorScheme="teal">
-        <TableCaption>
-          <Button
-            disabled={!hasNextPage}
-            variant="outline"
-            isLoading={isFetching && hasData}
-            onClick={() => {
-              fetchNextPage({ pageParam: nextCursor })
-            }}
-          >
-            {'Load more'}
-          </Button>
-        </TableCaption>
-        <Thead>
-          <Tr>
-            <Th>{t('time')}</Th>
-            <Th>{t('hash')}</Th>
-            <Th>{t('status')}</Th>
-          </Tr>
-        </Thead>
-        <Tbody>
-          {pages.map((page, idx) => {
-            return page.trxs.map((d, index) => {
-              const { node } = d
-              console.log(node)
-              return (
-                <Tr key={`${idx}-${index}`}>
-                  <Th>{timeformater(moment(node.time).unix())}</Th>
-                  <Th>{ellipseStr(node.hash)}</Th>
-                  <Th>{node.status}</Th>
-                </Tr>
-              )
-            })
-          })}
-        </Tbody>
-        {/* <Tfoot>
+    <>
+      {isLoading ? (
+        <LoadingPanel />
+      ) : (
+        <TableContainer>
+          <Table variant="striped" colorScheme="teal">
+            <TableCaption>
+              <Button
+                disabled={!hasNextPage}
+                variant="outline"
+                isLoading={isFetching && hasData}
+                onClick={() => {
+                  fetchNextPage({ pageParam: nextCursor })
+                }}
+              >
+                {'Load more'}
+              </Button>
+            </TableCaption>
+            <Thead>
+              <Tr>
+                <Th>{t('time')}</Th>
+                <Th>{t('hash')}</Th>
+                <Th>{t('status')}</Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              {pages.map((page = {}, idx) => {
+                const { trxs = [] } = page
+                return trxs.map((d, index) => {
+                  const { node } = d
+                  console.log(node)
+                  return (
+                    <Tr key={`${idx}-${index}`}>
+                      <Th>{timeformater(moment(node.time).unix())}</Th>
+                      <Th>{ellipseStr(node.hash)}</Th>
+                      <Th>{node.status}</Th>
+                    </Tr>
+                  )
+                })
+              })}
+            </Tbody>
+            {/* <Tfoot>
           <Tr>
             <Th>To convert</Th>
             <Th>into</Th>
             <Th isNumeric>multiply by</Th>
           </Tr>
         </Tfoot> */}
-      </Table>
-    </TableContainer>
+          </Table>
+        </TableContainer>
+      )}
+    </>
   )
 }
